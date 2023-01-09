371d9a549da174f8e80d8312f6d36edbb6b2662b
charuzain <charuabhizain@gmail.com>
2023-01-08 20:27:30 -0700

    initial commit

---
+++ BirthdayApp/firebase.js
@@ -0,0 +1,13 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCVPbR8xsJEVgvWTT5agaonauqCvJ3Ijc0",
+    authDomain: "birthdayapp-9b0a6.firebaseapp.com",
+    projectId: "birthdayapp-9b0a6",
+    storageBucket: "birthdayapp-9b0a6.appspot.com",
+    messagingSenderId: "863361332987",
+    appId: "1:863361332987:web:e3949665c8ff193dc9a25e"
+  };
+
+const app = initializeApp(firebaseConfig);
+
