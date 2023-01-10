5e08391be10f99bead1a7b88d1c1a8b1fd5d7bda
charuzain <charuabhizain@gmail.com>
2023-01-09 19:33:16 -0700

    user signin using firebase authentication

---
+++ BirthdayApp/login.js
@@ -0,0 +1,29 @@
+import {auth,signInWithEmailAndPassword} from "./firebase.js";
+
+$(document).ready(() => {
+  $('button').on('click', (e) => {
+    e.preventDefault();
+    console.log('click');
+
+
+    const email = e.target.form[0].value;
+    const password = e.target.form[1].value;
+    // console.log(email);
+    // console.log(password);
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user)
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage)
+    });
+  
+
+  });
+
+
+});
