aa8d57f91cf463152b0b9893b14ff4f4531e25a6
charuzain <charuabhizain@gmail.com>
2023-01-13 10:08:53 -0500

    BusinessList component created

---
+++ Ravenous/ravenous/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Business from '../Business/Business';
+// import './BusinessList.css'
+import './BusinessList.css';
+
+
+class BusinessList extends React.Component{
+  render() {
+    return (
+      
+    )
+  }
+}
